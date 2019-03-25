34c1998065c58ac4299f0e71b378e8ef0a9857b0
ayush-rusau <ayush.rusau@gmail.com>
2019-03-25 14:07:12 +0530

    returing HTML file

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  // res.send('<h1>Hello from Express!</h1>');
+  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
+
+//__dirname -> global varibale holding absolute path(Routes folder)
 
 module.exports = router;
