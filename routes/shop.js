01d10abeb162a10de13467f5ec78f153f7baae4e
ayush-rusau <ayush.rusau@gmail.com>
2019-03-26 15:04:12 +0530

    adding layout in Pug

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop'});
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 //__dirname -> global varibale holding absolute path(Routes folder)
