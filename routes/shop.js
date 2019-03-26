4ed667279d8d79411debb57f364ad9210c46c910
ayush-rusau <ayush.rusau@gmail.com>
2019-03-26 14:40:25 +0530

    Working with Pug

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const router = express.Router();
 const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // res.send('<h1>Hello from Express!</h1>');
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 //__dirname -> global varibale holding absolute path(Routes folder)
