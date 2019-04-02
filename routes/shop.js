e17de2b60f1399522a818f2ddfd6451470eee211
ayush-rusau <ayush.rusau@gmail.com>
2019-04-02 17:27:44 +0530

    MVC

--- routes/shop.js
+++ routes/shop.js
@@ -2,26 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
-  // res.send('<h1>Hello from Express!</h1>');
-  // console.log(adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getProducts);
 
 //__dirname -> global varibale holding absolute path(Routes folder)
 
