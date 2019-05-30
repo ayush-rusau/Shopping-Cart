aa55957a37a2a6d7e85973b8433dcb367cbf7d63
ayush-rusau <ayush.rusau@gmail.com>
2019-05-30 10:10:21 +0530

    enhancing the project

--- util/path.js
+++ util/path.js
@@ -1,8 +1,3 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = path.dirname(process.mainModule.filename);
-
-// process.mainModule.filename => 
-//         This refers to the file responsible for app running
-
-// dirname() => get the path to that dierectory
